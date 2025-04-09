34ab8d8efec01a8930cbf0ffb6ef28e01d03ca1a
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-09 12:06:02 +0200

    refactoring

--- src/main/scala/PythagoreanTriplet.scala
+++ src/main/scala/PythagoreanTriplet.scala
@@ -6,12 +6,8 @@ object PythagoreanTriplet:
       for
          a <- n1 to n2
          b <- a + 1 to n2
-         c <- a + 1 to n2 if isPythagorean(a, b, c)
+         c = math.sqrt(a * a + b * b).toInt if isPythagorean(a, b, c) && c <= n2
       yield (a, b, c)
 
    def pythagoreanTripletsSum(n: Int): Seq[(Int, Int, Int)] =
-      for
-         a <- 1 to n
-         b <- a + 1 to n
-         c = n - a - b if isPythagorean(a, b, c)
-      yield (a, b, c)
+      pythagoreanTriplets(1, n).filter(t => t._1 + t._2 + t._3 == n)
