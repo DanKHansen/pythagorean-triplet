a3f9f9291e667de0edd253ede73c5d27ef633aa7
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-09 11:05:01 +0200

    solution

--- src/main/scala/PythagoreanTriplet.scala
+++ src/main/scala/PythagoreanTriplet.scala
@@ -1,12 +1,14 @@
 object PythagoreanTriplet:
-   private lazy val pl = {
+   private lazy val pl =
       for
          a <- 1 to 300
          b <- 1 to 400
+         if a < b
          c <- 1 to 500
+         if b < c
+         if isPythagorean(a, b, c)
+         if a + b + c <= 1000
       yield (a, b, c)
-   }
-      .filter(t => t._1 < t._2 && t._2 < t._3 && isPythagorean(t) && t._1 + t._2 + t._3 <= 1000)
 
    def isPythagorean(t: (Int, Int, Int)): Boolean =
       t._1 * t._1 + t._2 * t._2 == t._3 * t._3
