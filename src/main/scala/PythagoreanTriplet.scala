cc3c0f2bda74e3354740d9f18704daf5adb7a673
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-09 11:51:00 +0200

    refactoring

--- src/main/scala/PythagoreanTriplet.scala
+++ src/main/scala/PythagoreanTriplet.scala
@@ -1,15 +1,17 @@
 object PythagoreanTriplet:
-   private lazy val pt =
-      for
-         a <- 1 to 300
-         b <- 1 to 400
-         if a < b
-         c <- 1 to 500
-         if b < c && isPythagorean(a, b, c) && a + b + c <= 1000
-      yield (a, b, c)
 
    def isPythagorean(t: (Int, Int, Int)): Boolean = t._1 * t._1 + t._2 * t._2 == t._3 * t._3
 
-   def pythagoreanTriplets(n1: Int, n2: Int): Seq[(Int, Int, Int)] = pt.filter(t => t._1 >= n1 && t._3 <= n2)
+   def pythagoreanTriplets(n1: Int, n2: Int): Seq[(Int, Int, Int)] =
+      for
+         a <- n1 to n2
+         b <- a + 1 to n2
+         c <- a + 1 to n2 if isPythagorean(a, b, c)
+      yield (a, b, c)
 
-   def pythagoreanTripletsSum(n: Int): Seq[(Int, Int, Int)] = pt.filter(t => t._1 + t._2 + t._3 == n)
+   def pythagoreanTripletsSum(n: Int): Seq[(Int, Int, Int)] =
+      for
+         a <- 1 to n
+         b <- a + 1 to n
+         c = n - a - b if isPythagorean(a, b, c)
+      yield (a, b, c)
